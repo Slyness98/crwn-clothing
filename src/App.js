16d67a2cd2f7df955db3573b7a62b5e98299f1bb
Slyness98 <sethlyness@outlook.com>
2020-07-22 17:41:35 -0700

    Added cart-icon and cart-dropdown components to the app. Also added new reducers and distributed redux store state as props to accomodate these new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import './App.css';
 
 class App extends React.Component {
 
@@ -42,15 +47,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signIn' component={SignInAndSignUpPage} />
+          <Route path='/signIn' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
    setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import './header.styles.scss';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div
      className="header">
     
@@ -21,19 +23,24 @@ const Header = ({currentUser}) => (
             CONTACT
         </Link>
         {
-            currentUser ? 
-            <div className='option' onClick={() => auth.signOut()}>
-                SIGN OUT
-            </div>
-            :
+        currentUser ? 
+        (<div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+        </div>
+        ) : (
             <Link className='option' to='/signIn'>SIGN IN</Link>
-        }
+        )}
+        <CartIcon />
       </div>
+      {
+          hidden ? null : <CartDropdown />
+      }
     </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden} }) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
